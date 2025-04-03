c0c25cf4277e6fe723fb768de9dd03566f1fab74
Johnny Kelly/Seáinín Ó Ceallaigh <119359970+seaininoceallaigh@users.noreply.github.com>
2025-04-02 22:22:26 -0400

    Update jiggle.js

--- jiggle.js
+++ jiggle.js
@@ -4,6 +4,7 @@ if (snapshot) {
   document.body.style.backgroundImage = `url(${snapshot})`;
   document.body.style.backgroundSize = 'cover';
   document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundPosition = 'center';
 }
 
   
