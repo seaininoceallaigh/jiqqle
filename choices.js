731bd60f1a266d7236a0f237bad66140d82c1ea4
Johnny Kelly/Seáinín Ó Ceallaigh <119359970+seaininoceallaigh@users.noreply.github.com>
2025-04-09 16:51:05 -0400

    Update choices.js

--- choices.js
+++ choices.js
@@ -129,7 +129,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
       document.getElementById('choices-section').style.display = 'block';
       initChoices();
-    }, 500);
+    }, 100);
   }
   
   // Select the appropriate loader callback based on the skip flag.
